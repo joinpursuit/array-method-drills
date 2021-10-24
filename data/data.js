86787391ccc7046ef0ab321dee76bc7907528dc1
Jeffrey <jeffreysebastian@pursuit.org>
2021-10-24 18:03:16 -0400

    missing arrayValuesTimes100TimesIndex

--- data/data.js
+++ data/data.js
@@ -1,5 +1,23 @@
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0];
 
+// nums.every(isEveryNumGreaterThan2())
+const isEveryNumGreaterThan2 = (number) =>{
+  return number >= 2
+}
+
+nums.every(isEveryNumGreaterThan2);
+
+
+function arrayLessThan5(){
+  nums.filter(num =>{
+    return num < 5
+  })
+}
+
+console.log(arrayLessThan5(nums))
+
+
+
 const words = [
   "The",
   "quick",
--- solution.js
+++ solution.js
@@ -1,62 +1,88 @@
 const { nums, words } = require("./data/data.js");
 
 // Every
-const isEveryNumGreaterThan2 = () => {
-  //
+const isEveryNumGreaterThan2 = (number) => {
+ return  number.every(num => {
+    return num >=2
+  })
 };
+
+isEveryNumGreaterThan2(nums)
 
 const isEveryWordShorterThan7 = () => {
-  //
+  return words.every(word => {
+    return word.length < 7
+  })
 };
-
+isEveryWordShorterThan7(words)
 // Filter
 
 const arrayLessThan5 = () => {
-  //
+  return nums.filter(num =>{
+    return num < 5
+  })
 };
 
 const arrayOddLengthWords = () => {
-  //
+  return words.filter(word => {
+    return word.length % 2 === 1
+  })
 };
 
 // Find
 
 const firstValDivisibleBy4 = () => {
-  //
+  return nums.find(num =>{
+    return num % 4 === 0
+  })
 };
 
 const firstWordLongerThan4Char = () => {
-  //
+  return words.find(word => {
+    return word.length > 4
+  })
 };
 
 // For Each
 
 const logValuesTimes3 = () => {
-  //
+  return nums.forEach(num =>{
+    num * 3
+  })
 };
 
 const logWordsWithExclamation = () => {
-  //
+  return words.forEach(word =>{
+    word + '!'
+  })
 };
 
 // Map
 
 const arrayValuesTimes100TimesIndex = () => {
-  //
+  return nums.every(num => {
+    return num * 100
+  })
 };
 
 const arrayWordsUpcased = () => {
-  //
+  return words.map(word => {
+    return word.toUpperCase()
+  })
 };
 
 // Some
 
 const areSomeNumsDivisibleBy7 = () => {
-  //
+return nums.some(num => {
+  return num % 7 === 0
+})
 };
 
 const doSomeWordsHaveAnA = () => {
-  //
+  return words.some(word => {
+    return word.indexOf('a')
+  })
 };
 
 module.exports = {
