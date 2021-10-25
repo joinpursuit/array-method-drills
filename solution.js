b85c527e841e58fc1d84cb989298d6c78e5036a0
Angelina Ebreo <angelinaebreo@gmail.com>
2021-10-25 13:47:40 -0400

    complete lab

--- solution.js
+++ solution.js
@@ -2,61 +2,62 @@ const { nums, words } = require("./data/data.js");
 
 // Every
 const isEveryNumGreaterThan2 = () => {
-  //
+  return nums.every(el => el > 2)
 };
 
 const isEveryWordShorterThan7 = () => {
   //
+  return words.every(el => el.length < 7)
 };
 
 // Filter
 
 const arrayLessThan5 = () => {
-  //
+  return nums.filter(el => el < 5)
 };
 
 const arrayOddLengthWords = () => {
-  //
+  return words.filter(el => el.length % 2 !== 0)
 };
 
 // Find
 
 const firstValDivisibleBy4 = () => {
-  //
+  return nums.find(el => el % 4 === 0)
 };
 
 const firstWordLongerThan4Char = () => {
-  //
+  return words.find(el => el.length > 4)
 };
 
 // For Each
 
 const logValuesTimes3 = () => {
-  //
+  return nums.forEach(el => console.log(el*3))
 };
 
 const logWordsWithExclamation = () => {
-  //
+  return words.forEach(el => console.log(el, "!"))
 };
 
 // Map
 
 const arrayValuesTimes100TimesIndex = () => {
-  //
+  return nums.map((el, i) => el*el*i)
 };
 
 const arrayWordsUpcased = () => {
-  //
+  return words.map(el => el.toUpperCase())
 };
 
 // Some
 
 const areSomeNumsDivisibleBy7 = () => {
-  //
+  return nums.some(el => el % 7 === 0)
 };
 
 const doSomeWordsHaveAnA = () => {
-  //
+  return words.some(el => el.includes("a"))
 };
 
 module.exports = {
