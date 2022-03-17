e949e549dbfe23f3f9b1a300d1d7136fa1c6f3e6
Christina Loiacono <loiaconochristina@gmail.com>
2022-03-17 18:29:52 -0400

    passing all tests

--- solution.js
+++ solution.js
@@ -34,41 +34,67 @@ const arrayOddLengthWords = () => {
 // Find
 
 const firstValDivisibleBy4 = () => {
-  //
+  let findFirstDivisibleByFour = nums.find((num)=>{
+    return num % 4 === 0;
+  })
+  return findFirstDivisibleByFour;
 };
 
 const firstWordLongerThan4Char = () => {
-  //
+  let firstLongerThanFourChar = words.find((word)=>{
+    return word.length > 4;
+  })
+  return firstLongerThanFourChar;
 };
 
 // For Each
-
 const logValuesTimes3 = () => {
-  //
+  // for (let num of nums){
+  //   console.log(num * 3)
+  // }
+  nums.forEach(num => console.log(num * 3));
 };
 
 const logWordsWithExclamation = () => {
-  //
+  words.forEach((word)=>{
+    console.log(word + "!");
+  })
 };
 
 // Map
 
 const arrayValuesTimes100TimesIndex = () => {
-  //
+  let answer = nums.map((num, index)=>{
+    return (num * num) * index;
+  })
+  return answer;
 };
 
 const arrayWordsUpcased = () => {
-  //
+  let arrayUpperCase = words.map((word)=>{
+    return word.toUpperCase();
+  })
+  return arrayUpperCase;
 };
 
 // Some
 
 const areSomeNumsDivisibleBy7 = () => {
-  //
+  let areSomeNumsDivisibleSeven = nums.some((num)=>{
+    if (num % 7 === 0){
+      return true;
+    } else {
+      return false;
+    }
+  })
+  return areSomeNumsDivisibleSeven;
 };
 
 const doSomeWordsHaveAnA = () => {
-  //
+  let doSomeWordsHaveA = words.some((word)=>{
+    return word.includes("a");
+  })
+  return doSomeWordsHaveA;
 };
 
 module.exports = {
