71047b65e242daeeead4d4c3393fa9121be0cb00
Rose Durandisse <rosedurandisse@pursuit.org>
2022-03-19 19:36:33 -0400

    MVP Solutions

--- solution.js
+++ solution.js
@@ -1,61 +1,68 @@
 const { nums, words } = require("./data/data.js");
 
 // Every
-const isEveryNumGreaterThan2 = () => {
-  //
+const isEveryNumGreaterThan2 = (nums) => {
+  return nums.every((eachElement) => eachElement > 2);
 };
 
-const isEveryWordShorterThan7 = () => {
-  //
+const isEveryWordShorterThan7 = (Words) => {
+  return words.every((eachElement) => eachElement.length < 7);
 };
 
 // Filter
-
-const arrayLessThan5 = () => {
+const arrayLessThan5 = (nums) => {
+  return nums.filter((eachElement) => eachElement < 5);
   //
 };
 
-const arrayOddLengthWords = () => {
+const arrayOddLengthWords = (words) => {
+  return words.filter((eachElement) => eachElement.length % 2 === 1);
   //
 };
 
 // Find
 
-const firstValDivisibleBy4 = () => {
+const firstValDivisibleBy4 = (nums) => {
+  return nums.find((eachElement) => eachElement % 4 === 0);
   //
 };
 
-const firstWordLongerThan4Char = () => {
-  //
+const firstWordLongerThan4Char = (words) => {
+  return words.find((eachElement) => eachElement.length > 4);
 };
 
 // For Each
-
-const logValuesTimes3 = () => {
+const logValuesTimes3 = (nums) => {
+  return nums.forEach((eachElement) => console.log(eachElement * 3));
   //
 };
 
-const logWordsWithExclamation = () => {
+const logWordsWithExclamation = (words) => {
+  return words.forEach((eachElement) => eachElement.includes("!"));
   //
 };
 
 // Map
 
-const arrayValuesTimes100TimesIndex = () => {
+const arrayValuesTimes100TimesIndex = (nums) => {
+  return nums.map((eachElement, index) => eachElement * index * eachElement);
   //
 };
 
-const arrayWordsUpcased = () => {
+const arrayWordsUpcased = (words) => {
+  return words.map((eachElement) => eachElement.toUpperCase());
   //
 };
 
 // Some
 
-const areSomeNumsDivisibleBy7 = () => {
+const areSomeNumsDivisibleBy7 = (nums) => {
+  return nums.some((eachElement) => eachElement % 7 === 0);
   //
 };
 
-const doSomeWordsHaveAnA = () => {
+const doSomeWordsHaveAnA = (words) => {
+  return words.some((eachElement) => eachElement.includes("a"));
   //
 };
 
