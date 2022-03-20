172a951f58385e27b5fb5700d4e286e7993df7e1
Angie G <angieg@Angies-MBP.fios-router.home>
2022-03-20 14:12:59 -0400

    Add File

--- solution.js
+++ solution.js
@@ -1,22 +1,28 @@
 const { nums, words } = require("./data/data.js");
 
 // Every
+
+// 1. Determine if every number is greater than or equal to 2
 const isEveryNumGreaterThan2 = () => {
     return nums.every((num) => num > 2)
 };
 
+// 1. determine if every word shorter than 7 characters
 const isEveryWordShorterThan7 = () => {
     return words.every((word) => word.length < 7)
 };
 
 // Filter
 
+// 1. filter the array for numbers less than 5
 const arrayLessThan5 = () => {
-  //
+    return nums.filter((num) => num < 5)
 };
 
+// 1. filter words that have an odd length
 const arrayOddLengthWords = () => {
-//
+    return words.filter((word) => word.length % 2 === 1)
+
 };
 
 // Find
