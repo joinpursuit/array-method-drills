6113d855fad910400dcdb9bb369b830ac2866bc3
Angie G <angieg@Angies-MBP.fios-router.home>
2022-03-20 14:09:00 -0400

    Every() Method

--- solution.js
+++ solution.js
@@ -2,11 +2,11 @@ const { nums, words } = require("./data/data.js");
 
 // Every
 const isEveryNumGreaterThan2 = () => {
-  //
+    return nums.every((num) => num > 2)
 };
 
 const isEveryWordShorterThan7 = () => {
-  //
+    return words.every((word) => word.length < 7)
 };
 
 // Filter
@@ -16,47 +16,47 @@ const arrayLessThan5 = () => {
 };
 
 const arrayOddLengthWords = () => {
-  //
+//
 };
 
 // Find
 
 const firstValDivisibleBy4 = () => {
-  //
+//
 };
 
 const firstWordLongerThan4Char = () => {
-  //
+//
 };
 
 // For Each
 
 const logValuesTimes3 = () => {
-  //
+//
 };
 
 const logWordsWithExclamation = () => {
-  //
+//
 };
 
 // Map
 
 const arrayValuesSquaredTimesIndex = () => {
-  //
+//
 };
 
 const arrayWordsUpcased = () => {
-  //
+//
 };
 
 // Some
 
 const areSomeNumsDivisibleBy7 = () => {
-  //
+//
 };
 
 const doSomeWordsHaveAnA = () => {
-  //
+//
 };
 
 module.exports = {
