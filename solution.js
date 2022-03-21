3f1bea6d1902a197d66b32dd57cb42fe340153be
Taylor W <taylorwilliams@pursuit.org>
2022-03-20 21:34:49 -0400

    10 problems complete

--- solution.js
+++ solution.js
@@ -1,61 +1,97 @@
 const { nums, words } = require("./data/data.js");
 
+console.log(words);
 // Every
-const isEveryNumGreaterThan2 = () => {
-  //
+const isEveryNumGreaterThan2 = (number) => {
+  let greater = number.find((n) => n >= 2);
+  return greater;
+};
+//
+
+isEveryNumGreaterThan2(nums);
+
+const isEveryWordShorterThan7 = (array) => {
+  let filtered = array.filter((a) => a.length > 7);
+  console.log(array);
+  return filtered;
 };
 
-const isEveryWordShorterThan7 = () => {
-  //
-};
+isEveryWordShorterThan7(words);
+
+// isEveryWordShorterThan7(words);
+//
+// console.log(words.every(isEveryWordShorterThan7));
 
 // Filter
 
-const arrayLessThan5 = () => {
+const arrayLessThan5 = (array) => {
+  let lessThan5 = array.filter((arr) => arr < 5);
+  return lessThan5;
   //
 };
 
-const arrayOddLengthWords = () => {
+arrayLessThan5(words);
+
+const arrayOddLengthWords = (letters) => {
+  let oddWords = letters.filter((letter) => letter.length % 2);
+  return oddWords;
   //
 };
 
 // Find
 
-const firstValDivisibleBy4 = () => {
+const firstValDivisibleBy4 = (value) => {
+  let firstVal = value.find((val) => val % 4 === 0);
+  return firstVal;
   //
 };
 
-const firstWordLongerThan4Char = () => {
+const firstWordLongerThan4Char = (character) => {
+  let longChar = character.find((char) => char.length > 4);
+  return longChar;
   //
 };
 
 // For Each
 
-const logValuesTimes3 = () => {
+const logValuesTimes3 = (digits) => {
+  let arr = digits.forEach((digs) => digs * 3);
+  return arr;
   //
 };
 
-const logWordsWithExclamation = () => {
+const logWordsWithExclamation = (words) => {
+  return words.forEach((word) => word + "!");
   //
 };
 
 // Map
 
-const arrayValuesTimes100TimesIndex = () => {
+const arrayValuesTimes100TimesIndex = (values) => {
   //
+  console.log(
+    values.map((value) => {
+      value * 3;
+    })
+  );
 };
 
-const arrayWordsUpcased = () => {
+console.log(arrayValuesTimes100TimesIndex(nums));
+
+const arrayWordsUpcased = (char) => {
+  return char.map((c) => c.toUpperCase());
   //
 };
 
 // Some
 
-const areSomeNumsDivisibleBy7 = () => {
+const areSomeNumsDivisibleBy7 = (numbers) => {
+  return numbers.some((num) => num % 7);
   //
 };
 
-const doSomeWordsHaveAnA = () => {
+const doSomeWordsHaveAnA = (words) => {
+  return words.some((word) => word.includes("a"));
   //
 };
 
