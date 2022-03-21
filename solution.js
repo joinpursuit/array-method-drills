97f852ce65b9dd47e45e795b65e191a15edd8f9d
Larisa <larisashapkinab@gmail.com>
2022-03-20 22:10:12 -0400

    Passed all the tests

--- solution.js
+++ solution.js
@@ -1,62 +1,65 @@
 const { nums, words } = require("./data/data.js");
 
 // Every
-const isEveryNumGreaterThan2 = () => {
-  //
+const isEveryNumGreaterThan2 = (nums) => {
+  return nums.every((num)=>{
+     num>=2;
+  })
 };
 
 const isEveryWordShorterThan7 = () => {
-  //
+   return words.every(word=>word.length<7)
 };
 
 // Filter
 
 const arrayLessThan5 = () => {
-  //
+  return nums.filter(num=>num<5)
 };
 
 const arrayOddLengthWords = () => {
-  //
+  return words.filter(word=>word.length%2===1)
 };
 
 // Find
 
 const firstValDivisibleBy4 = () => {
-  //
+  return nums.find(num=>num%4===0)
 };
 
 const firstWordLongerThan4Char = () => {
-  //
+  return words.find(word=>word.length>4)
 };
 
 // For Each
 
 const logValuesTimes3 = () => {
-  //
+  console.log(words.forEach(num=>num*3)) 
 };
 
 const logWordsWithExclamation = () => {
-  //
+  console.log(words.forEach(word=>word+'!'))
 };
 
 // Map
 
 const arrayValuesTimes100TimesIndex = () => {
-  //
+  return nums.map((num,index)=>{
+    return num*num*index})
 };
 
 const arrayWordsUpcased = () => {
-  //
+  return words.map(word=>word.toUpperCase())
 };
 
 // Some
 
 const areSomeNumsDivisibleBy7 = () => {
-  //
+  return nums.some(num=>num%7===0)
 };
 
 const doSomeWordsHaveAnA = () => {
-  //
+  return words.some(word=>word+'a')
 };
 
 module.exports = {
