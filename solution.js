49c29260b6d94954cc03bcf9edb075411544cee4
David Vidals <david.vidals08@gmail.com>
2022-03-21 04:22:55 -0400

    finished Lab

--- solution.js
+++ solution.js
@@ -2,61 +2,73 @@ const { nums, words } = require("./data/data.js");
 
 // Every
 const isEveryNumGreaterThan2 = () => {
+return nums.every(curr => curr >2)
   //
 };
 
 const isEveryWordShorterThan7 = () => {
   //
+  return words.every(w => w.length < 7)
 };
 
 // Filter
 
 const arrayLessThan5 = () => {
   //
+  return nums.filter(x=> x<5)
 };
 
 const arrayOddLengthWords = () => {
   //
+  return words.filter(x => x.length%2)
 };
 
 // Find
 
 const firstValDivisibleBy4 = () => {
   //
+  return nums.find(x=> x%4===0)
 };
 
 const firstWordLongerThan4Char = () => {
   //
+  return words.find(w=>w.length > 4)
 };
 
 // For Each
 
 const logValuesTimes3 = () => {
   //
+  return nums.forEach(x=>x *3)
 };
 
 const logWordsWithExclamation = () => {
   //
+  return words.forEach(w=>w +'!')
 };
 
 // Map
 
 const arrayValuesTimes100TimesIndex = () => {
   //
+  return nums.map((x,i) => x*x*i)
 };
 
 const arrayWordsUpcased = () => {
-  //
+  // 
+  return words.map(w=>w.toUpperCase())
 };
 
 // Some
 
 const areSomeNumsDivisibleBy7 = () => {
   //
+  return nums.some(x=>x%7)
 };
 
 const doSomeWordsHaveAnA = () => {
   //
+ return words.some(w=> w.includes('a'))
 };
 
 module.exports = {
