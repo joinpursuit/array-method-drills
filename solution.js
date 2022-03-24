fe10d34f0022b31d1d44c1329d08129e05d8b30b
Stephanie <stephaniereyes@pursuit.org>
2022-03-23 20:28:36 -0400

    got stuck on the last one

--- solution.js
+++ solution.js
@@ -1,62 +1,102 @@
 const { nums, words } = require("./data/data.js");
 
 // Every
-const isEveryNumGreaterThan2 = () => {
-  //
-};
+const isEveryNumGreaterThan2 = (nums) => {
+  let greatThan = nums.every((el, ind)=>{
+   return el > 2
+ })
+return greatThan
+}
+
 
 const isEveryWordShorterThan7 = () => {
-  //
+ let shortWord = words.every((el, ind)=>{
+  return el.length < 7 
+})
+return shortWord
 };
 
 // Filter
 
 const arrayLessThan5 = () => {
-  //
+ let lessThan = nums.filter((element)=>{
+  return element < 5
+  })
+  return lessThan
 };
 
 const arrayOddLengthWords = () => {
-  //
+  let odd = words.filter((el)=>{
+    return el.length % 2 === 1
+  })
+  return odd
 };
 
 // Find
 
 const firstValDivisibleBy4 = () => {
-  //
+  let val = nums.find((el)=>{
+    return el >= 4
+  })
+  return val
 };
 
 const firstWordLongerThan4Char = () => {
-  //
-};
+  let four= words.find((el)=>{
+   return el.length > 4
+ })
+ return four
+}
 
 // For Each
 
 const logValuesTimes3 = () => {
-  //
+ let logIn =  nums.forEach((el)=>{
+   return el * 3
+  })
+  return logIn
 };
 
 const logWordsWithExclamation = () => {
-  //
+  // let newArr = []
+  let exc = words.forEach((el)=>{
+    return el += "!"
+    // return newArr.push(el += "!")
+  })
+  return exc
 };
 
 // Map
 
 const arrayValuesTimes100TimesIndex = () => {
-  //
+  let newNum = nums.map((el,i )=>{
+    return el * el * i
+  })
+   return newNum
 };
 
 const arrayWordsUpcased = () => {
-  //
+  let newW = words.map((el)=>{
+    return el.toUpperCase()
+  })
+  return newW
 };
 
 // Some
 
 const areSomeNumsDivisibleBy7 = () => {
-  //
+  let bySev = nums.some((el)=>{
+    return el /7
+  })
+  return bySev
 };
 
 const doSomeWordsHaveAnA = () => {
-  //
+  let word = words.some((el)=>{
+    return el === "a"
+  })
+  return word
+
 };
 
 module.exports = {
