052cdc20eccd7bf9f522f4e246f43e21dba3ad34
Milton Clark <kingcoder247@gmail.com>
2022-03-28 09:42:12 -0400

    Passed first 5 tests and completed the functions for them correctly

--- solution.js
+++ solution.js
@@ -1,27 +1,64 @@
 const { nums, words } = require("./data/data.js");
 
-// Every
-const isEveryNumGreaterThan2 = () => {
-  //
+
+const isEveryNumGreaterThan2 = (nums) => {
+  let results = nums.every((num)=>{
+    if(num >= 2){
+      return true;
+    } else{
+      return false;
+    }
+  });
+  return results;
 };
 
-const isEveryWordShorterThan7 = () => {
+const isEveryWordShorterThan7 = (words) => {
+  let results = words.every((word)=>{
+    if(word.length < 7){
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return results;
   //
 };
 
 // Filter
 
-const arrayLessThan5 = () => {
-  //
+const arrayLessThan5 = (nums) => {
+  let result = nums.filter((num)=>{
+    if(num < 5){
+      return true;
+    } else{
+      return false;
+    }
+    });
+  return result;
 };
 
-const arrayOddLengthWords = () => {
-  //
+const arrayOddLengthWords = (words) => {
+  let result = words.filter((word)=>{
+    if(word.length % 2){
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return result;
 };
 
 // Find
 
-const firstValDivisibleBy4 = () => {
+const firstValDivisibleBy4 = (nums) => {
+  let results = nums.find((num)=>{
+    if(num % 4 === 0){
+      return true;
+    } else{
+      return false;
+    }
+  })
+  return results;
   //
 };
 
