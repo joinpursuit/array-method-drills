15e7ffd1d98a4250e68a8952b98e4ea3b03cd2d9
Jonathan Scheiber <jon@scheiber.me>
2022-09-23 13:43:57 -0400

    complete array method drills

--- solution.js
+++ solution.js
@@ -1,73 +1,48 @@
 const { nums, words } = require("./data/data.js");
 
 // Every
-const isEveryNumGreaterThan2 = () => {
-  //
-};
 
-const isEveryWordShorterThan7 = () => {
-  //
-};
+const isEveryNumGreaterThan2 = () => nums.every((num) => num >= 2);
+
+const isEveryWordShorterThan7 = () => words.every((word) => word.length < 7);
 
 // Filter
 
-const arrayLessThan5 = () => {
-  //
-};
+const arrayLessThan5 = () => nums.filter((num) => num < 5);
 
-const arrayOddLengthWords = () => {
-  //
-};
+const arrayOddLengthWords = () => words.filter((word) => word.length % 2 == 1);
 
 // Find
 
-const firstValDivisibleBy4 = () => {
-  //
-};
+const firstValDivisibleBy4 = () => nums.find((num) => num % 4 === 0);
 
-const firstWordLongerThan4Char = () => {
-  //
-};
+const firstWordLongerThan4Char = () => words.find((word) => word.length > 4);
 
 // Find Index
 
-const firstNumIndexDivisibleBy3 = () => {
-  //
-};
+const firstNumIndexDivisibleBy3 = () => nums.findIndex((num) => num % 3 === 0);
 
-const firstWordIndexLessThan2Char = () => {
-  //
-};
+const firstWordIndexLessThan2Char = () =>
+  words.findIndex((word) => word.length < 2);
 
 // For Each
 
-const logValuesTimes3 = () => {
-  //
-};
+const logValuesTimes3 = () => nums.forEach((num) => console.log(num * 3));
 
-const logWordsWithExclamation = () => {
-  //
-};
+const logWordsWithExclamation = () =>
+  words.forEach((word) => console.log(`${word}!`));
 
 // Map
 
-const arrayValuesSquaredTimesIndex = () => {
-  //
-};
+const arrayValuesSquaredTimesIndex = () => nums.map((num, i) => num * num * i);
 
-const arrayWordsUpcased = () => {
-  //
-};
+const arrayWordsUpcased = () => words.map((word) => word.toUpperCase());
 
 // Some
 
-const areSomeNumsDivisibleBy7 = () => {
-  //
-};
+const areSomeNumsDivisibleBy7 = () => nums.some((num) => num % 7 === 0);
 
-const doSomeWordsHaveAnA = () => {
-  //
-};
+const doSomeWordsHaveAnA = () => words.some((word) => word.includes("a"));
 
 module.exports = {
   isEveryNumGreaterThan2,
