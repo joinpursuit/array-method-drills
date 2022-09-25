77c223d94d5d6fa70b8a67f334033f8e4bf2acb9
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2022-09-25 19:01:55 -0400

    changes to parameters

--- solution.js
+++ solution.js
@@ -19,8 +19,8 @@ const isEveryNumGreaterThan2 = (nums) => {
   return nums.every(num => num > 2)
 };
 
-const isEveryWordShorterThan7 = (panagram) => {
-  return panagram.every(word => word.length < 7)
+const isEveryWordShorterThan7 = (words) => {
+  return words.every(word => word.length < 7)
 };
 
 // Filter
@@ -28,8 +28,8 @@ const arrayLessThan5 = (nums) => {
   return nums.filter(num => num < 5)
 };
 
-const arrayOddLengthWords = (panagram) => {
-  return panagram.filter(word => word.length % 2 === 1)
+const arrayOddLengthWords = (words) => {
+  return words.filter(word => word.length % 2 === 1)
 };
 
 // Find
@@ -37,8 +37,8 @@ const firstValDivisibleBy4 = (nums) => {
   return nums.find(num => num % 4 === 0)
 };
 
-const firstWordLongerThan4Char = (panagram) => {
-  return panagram.find(word => word.length > 4)
+const firstWordLongerThan4Char = (words) => {
+  return words.find(word => word.length > 4)
 };
 
 // Find Index
@@ -46,8 +46,8 @@ const firstNumIndexDivisibleBy3 = (nums) => {
   return nums.findIndex(num => num % 3 === 0)
 };
 
-const firstWordIndexLessThan2Char = (panagram) => {
-  return panagram.findIndex(word => word.length < 2)
+const firstWordIndexLessThan2Char = (words) => {
+  return words.findIndex(word => word.length < 2)
 };
 
 // For Each
@@ -55,8 +55,8 @@ const logValuesTimes3 = (nums) => {
   nums.forEach(num => console.log(num * 3))
 };
 
-const logWordsWithExclamation = (panagram) => {
-  panagram.forEach(word => console.log(word + "!"))
+const logWordsWithExclamation = (words) => {
+  words.forEach(word => console.log(word + "!"))
 };
 
 // Map
@@ -64,8 +64,8 @@ const arrayValuesSquaredTimesIndex = (nums) => {
   return nums.map((num, index) => (num ** 2) * index)
 };
 
-const arrayWordsUpcased = (panagram) => {
-  return panagram.map(word => word.toUpperCase())
+const arrayWordsUpcased = (words) => {
+  return words.map(word => word.toUpperCase())
 };
 
 // Some
@@ -73,8 +73,8 @@ const areSomeNumsDivisibleBy7 = (nums) => {
   return nums.some(num => num % 7 === 0)
 };
 
-const doSomeWordsHaveAnA = (panagram) => {
-  return panagram.some(word => word.includes("a"))
+const doSomeWordsHaveAnA = (words) => {
+  return words.some(word => word.includes("a"))
 };
 
 module.exports = {
