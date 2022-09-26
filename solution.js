caf6fd98617184a512efea301e48af9bd988f378
Christine Taylor <ctay47@icloud.com>
2022-09-26 10:04:24 -0400

    completed lab

--- solution.js
+++ solution.js
@@ -1,72 +1,72 @@
 const { nums, words } = require("./data/data.js");
 
 // Every
-const isEveryNumGreaterThan2 = () => {
-  //
+const isEveryNumGreaterThan2 = (nums) => {
+  return nums.every(number => number >= 2)
+  
 };
 
-const isEveryWordShorterThan7 = () => {
-  //
+const isEveryWordShorterThan7 = (words) => {
+  return words.every(word => word !== 7 )
 };
 
 // Filter
 
-const arrayLessThan5 = () => {
-  //
+const arrayLessThan5 = (nums) => {
+  return nums.filter(number => number <5)
 };
-
-const arrayOddLengthWords = () => {
-  //
+const arrayOddLengthWords = (words) => {
+ return words.filter(word => word.length % 2 === 1)
 };
 
 // Find
 
-const firstValDivisibleBy4 = () => {
-  //
+const firstValDivisibleBy4 = (nums) => {
+  return nums.find(number => number % 4 === 0)
 };
 
 const firstWordLongerThan4Char = () => {
-  //
+ return words.find(word => word.length > 4)
 };
 
 // Find Index
 
-const firstNumIndexDivisibleBy3 = () => {
-  //
+const firstNumIndexDivisibleBy3 = (nums) => {
+  return nums.findIndex((number) => number % 3 === 0);
 };
 
-const firstWordIndexLessThan2Char = () => {
-  //
+const firstWordIndexLessThan2Char = (words) => {
+return words.findIndex((word) => word < 2);
 };
 
 // For Each
 
-const logValuesTimes3 = () => {
-  //
+const logValuesTimes3 = (nums) => {
+  nums.forEach(number => console.log(number * 3));
 };
 
-const logWordsWithExclamation = () => {
-  //
+const logWordsWithExclamation = (words) => {
+  words.forEach(word => console.log(word + '!'));
 };
 
 // Map
 
-const arrayValuesSquaredTimesIndex = () => {
-  //
+const arrayValuesSquaredTimesIndex = (nums) => {
+  return nums.map((number, i) => (number ** 2) * i);
 };
 
-const arrayWordsUpcased = () => {
-  //
+const arrayWordsUpcased = (words) => {
+  return words.map((word) => word.toUpperCase());
 };
 
 // Some
 
-const areSomeNumsDivisibleBy7 = () => {
-  //
+const areSomeNumsDivisibleBy7 = (nums) => {
+  return nums.some((number) => number % 7 === 0);
 };
 
-const doSomeWordsHaveAnA = () => {
-  //
+const doSomeWordsHaveAnA = (words) => {
+  return words.some((word) => word.toLocaleLowerCase().includes('a'));
 };
 
 module.exports = {
