99742dd746c25d07048aa6fbd440f1c0b396378d
Andres Varon <anvaron@gmail.com>
2022-09-27 12:56:47 -0400

    Find: Find the first value divisible by 4 - tested

--- solution.js
+++ solution.js
@@ -30,9 +30,12 @@ const arrayOddLengthWords = (words) => {
 
 /***************************************************
  * Find
+ * The find() method returns the first element in the provided array that satisfies 
+ * the provided testing function. If no values satisfy the testing function, 
+ * undefined is returned.
  ***************************************************/
-const firstValDivisibleBy4 = () => {
-  //
+const firstValDivisibleBy4 = (nums) => {
+  return nums.find(n => n % 4 === 0);
 };
 
 const firstWordLongerThan4Char = () => {
