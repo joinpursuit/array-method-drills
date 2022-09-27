73dc9fdb7843f12ace9443976c2d1b7d6ace4e9d
sabrina <sabrinaescobarflores@gmail.com>
2022-09-27 19:33:01 -0400

    lab completed

--- solution.js
+++ solution.js
@@ -2,71 +2,72 @@ const { nums, words } = require("./data/data.js");
 
 // Every
 const isEveryNumGreaterThan2 = () => {
-  //
+ 
+ return nums.every(num =>  num >= 2);
 };
 
 const isEveryWordShorterThan7 = () => {
-  //
+  return words.every(word => word.length < 7);
 };
 
 // Filter
 
 const arrayLessThan5 = () => {
-  //
+  return nums.filter(num => num < 5);
 };
 
 const arrayOddLengthWords = () => {
-  //
+  return words.filter(word => word.length%2 !== 0);
 };
 
 // Find
 
 const firstValDivisibleBy4 = () => {
-  //
+ return nums.find(num => num%4 === 0 );
 };
 
 const firstWordLongerThan4Char = () => {
-  //
+  return words.find(word => word.length > 4 );
 };
 
 // Find Index
 
 const firstNumIndexDivisibleBy3 = () => {
-  //
+ return nums.findIndex(num => num%3 === 0);
 };
 
 const firstWordIndexLessThan2Char = () => {
-  //
+  return words.findIndex(word => word < 2)
 };
 
 // For Each
 
 const logValuesTimes3 = () => {
-  //
+  console.log(nums.forEach(num => num * 4));
 };
 
 const logWordsWithExclamation = () => {
-  //
+  console.log(words.forEach(word => `!${word}`))
 };
 
 // Map
 
 const arrayValuesSquaredTimesIndex = () => {
-  //
+  return nums.map((num , i) => num * num * i);
 };
 
 const arrayWordsUpcased = () => {
-  //
+ return words.map(word => word.toUpperCase())
 };
 
 // Some
 
 const areSomeNumsDivisibleBy7 = () => {
-  //
+  return nums.some(num => num%7 === 0)
 };
 
 const doSomeWordsHaveAnA = () => {
-  //
+   return words.some(word => word.includes("a"));
 };
 
 module.exports = {
