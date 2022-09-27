0ef1cfa497dbe8cc1d58030a75744c61004f8d8c
Edwin Codrington <edwincodrington@gmail.com>
2022-09-27 17:04:13 -0400

    lab solution

--- solution.js
+++ solution.js
@@ -1,87 +1,119 @@
 const { nums, words } = require("./data/data.js");
 
 // Every
-const isEveryNumGreaterThan2 = () => {
-  //
+const isEveryNumGreaterThan2 = (array) => {
+	return array.every((each) => {
+		return each > 2;
+	});
+	//
 };
 
-const isEveryWordShorterThan7 = () => {
-  //
+const isEveryWordShorterThan7 = (array) => {
+	return array.every((each) => {
+		return each.length < 7;
+	});
+	//
 };
 
 // Filter
 
-const arrayLessThan5 = () => {
-  //
+const arrayLessThan5 = (array) => {
+	return array.filter((each) => {
+		return each < 5;
+	});
+	//
 };
 
-const arrayOddLengthWords = () => {
-  //
+const arrayOddLengthWords = (array) => {
+	return array.filter((each) => {
+		return each.length % 2;
+	});
+	//
 };
 
 // Find
 
-const firstValDivisibleBy4 = () => {
-  //
+const firstValDivisibleBy4 = (array) => {
+	return array.find((each) => {
+		return each % 4 === 0;
+	});
+	//
 };
 
-const firstWordLongerThan4Char = () => {
-  //
+const firstWordLongerThan4Char = (array) => {
+	return array.find((each) => each.length > 4);
+	//
 };
 
 // Find Index
 
-const firstNumIndexDivisibleBy3 = () => {
-  //
+const firstNumIndexDivisibleBy3 = (array) => {
+	return array.findIndex((each) => each % 3 === 0);
+	//
 };
 
-const firstWordIndexLessThan2Char = () => {
-  //
+const firstWordIndexLessThan2Char = (array) => {
+	//
+	return array.findIndex((each) => each.length < 2);
 };
 
 // For Each
 
-const logValuesTimes3 = () => {
-  //
+const logValuesTimes3 = (array) => {
+	//
+	array.forEach((each) => {
+		console.log(each * 3);
+	});
 };
 
-const logWordsWithExclamation = () => {
-  //
+const logWordsWithExclamation = (array) => {
+	//
+	array.forEach((each) => {
+		console.log(`${each}!`);
+	});
 };
 
 // Map
 
-const arrayValuesSquaredTimesIndex = () => {
-  //
+const arrayValuesSquaredTimesIndex = (array) => {
+	//
+	return array.map((each, index) => {
+		return each ** 2 * index;
+	});
 };
 
-const arrayWordsUpcased = () => {
-  //
+const arrayWordsUpcased = (array) => {
+	//
+	return array.map((each, index) => {
+		return each.toUpperCase();
+	});
 };
 
 // Some
 
-const areSomeNumsDivisibleBy7 = () => {
-  //
+const areSomeNumsDivisibleBy7 = (array) => {
+	//
+	return array.some((each) => each % 7 === 0);
 };
 
-const doSomeWordsHaveAnA = () => {
-  //
+const doSomeWordsHaveAnA = (array) => {
+	//
+	return array.some((each) => each.includes("a"));
 };
 
 module.exports = {
-  isEveryNumGreaterThan2,
-  isEveryWordShorterThan7,
-  arrayLessThan5,
-  arrayOddLengthWords,
-  firstValDivisibleBy4,
-  firstWordLongerThan4Char,
-  firstNumIndexDivisibleBy3,
-  firstWordIndexLessThan2Char,
-  logValuesTimes3,
-  logWordsWithExclamation,
-  arrayValuesSquaredTimesIndex,
-  arrayWordsUpcased,
-  areSomeNumsDivisibleBy7,
-  doSomeWordsHaveAnA,
+	isEveryNumGreaterThan2,
+	isEveryWordShorterThan7,
+	arrayLessThan5,
+	arrayOddLengthWords,
+	firstValDivisibleBy4,
+	firstWordLongerThan4Char,
+	firstNumIndexDivisibleBy3,
+	firstWordIndexLessThan2Char,
+	logValuesTimes3,
+	logWordsWithExclamation,
+	arrayValuesSquaredTimesIndex,
+	arrayWordsUpcased,
+	areSomeNumsDivisibleBy7,
+	doSomeWordsHaveAnA,
 };
