756e02d5b5481920012afa81a6e64564eb1f7114
H.C.Guerrero <clydeguerrero1@gmail.com>
2022-09-27 21:56:53 -0400

    Lab submission

--- solution.js
+++ solution.js
@@ -1,71 +1,93 @@
-const { nums, words } = require("./data/data.js");
+const { nums, words } = require('./data/data.js');
 
 // Every
-const isEveryNumGreaterThan2 = () => {
+const isEveryNumGreaterThan2 = (nums) => {
+  return nums.every((num) => num >= 2, {});
   //
 };
 
-const isEveryWordShorterThan7 = () => {
+const isEveryWordShorterThan7 = (words) => {
+  return words.every((word) => word.length < 7, {});
   //
 };
 
 // Filter
 
-const arrayLessThan5 = () => {
+const arrayLessThan5 = (nums) => {
+  return nums.filter((num) => num < 5, {});
   //
 };
 
-const arrayOddLengthWords = () => {
+const arrayOddLengthWords = (words) => {
+  return words.filter((word) => word.length % 2 !== 0, {});
   //
 };
 
 // Find
 
-const firstValDivisibleBy4 = () => {
+const firstValDivisibleBy4 = (nums) => {
   //
+  let numIndex = nums.find((num) => num % 4 === 0, {});
+  return numIndex;
 };
 
-const firstWordLongerThan4Char = () => {
+const firstWordLongerThan4Char = (words) => {
   //
+  return words.find((word) => word.length > 4, {});
 };
 
 // Find Index
 
-const firstNumIndexDivisibleBy3 = () => {
+const firstNumIndexDivisibleBy3 = (nums) => {
+  return nums.findIndex((num) => num % 3 === 0, {});
+
   //
 };
 
-const firstWordIndexLessThan2Char = () => {
+const firstWordIndexLessThan2Char = (words) => {
+  return words.findIndex((word) => word.length < 2, {});
   //
 };
 
 // For Each
 
-const logValuesTimes3 = () => {
+const logValuesTimes3 = (nums) => {
+  nums.forEach((num) => {
+    console.log(num * 3);
+  });
   //
 };
 
-const logWordsWithExclamation = () => {
+const logWordsWithExclamation = (words) => {
+  words.forEach((word) => {
+    console.log(word + '!');
+  });
   //
 };
 
 // Map
 
-const arrayValuesSquaredTimesIndex = () => {
+const arrayValuesSquaredTimesIndex = (nums) => {
+  let squaredArray = nums.map((num, i) => Math.pow(num, 2) * i);
+  return squaredArray;
   //
 };
 
-const arrayWordsUpcased = () => {
+const arrayWordsUpcased = (words) => {
+  let capitalizedArray = words.map((word) => word.toUpperCase());
+  return capitalizedArray;
   //
 };
 
 // Some
 
-const areSomeNumsDivisibleBy7 = () => {
+const areSomeNumsDivisibleBy7 = (nums) => {
+  return nums.some((num) => num % 7 === 0, {});
   //
 };
 
-const doSomeWordsHaveAnA = () => {
+const doSomeWordsHaveAnA = (words) => {
+  return words.some((word) => word.charAt('a'), {});
   //
 };
 
