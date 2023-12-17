6e1a9e14f87d6ea62cd9702d454b700ccd76d445
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-12-17 15:10:48 -0500

    Complete 'Every', 'Filter'.

--- solution.js
+++ solution.js
@@ -1,87 +1,87 @@
-const { nums, words } = require("./data/data.js");
+const { nums, words } = require('./data/data.js');
 
 // Every
-const isEveryNumGreaterThan2 = () => {
-  //
+const isEveryNumGreaterThan2 = (nums) => {
+	return nums.every((num) => num >= 2);
 };
 
-const isEveryWordShorterThan7 = () => {
-  //
+const isEveryWordShorterThan7 = (words) => {
+	return words.every((word) => word.length < 7);
 };
 
 // Filter
 
-const arrayLessThan5 = () => {
-  //
+const arrayLessThan5 = (nums) => {
+	return nums.filter((num) => num < 5);
 };
 
-const arrayOddLengthWords = () => {
-  //
+const arrayOddLengthWords = (words) => {
+	return words.filter((word) => word.length % 2 !== 0);
 };
 
 // Find
 
 const firstValDivisibleBy4 = () => {
-  //
+	//
 };
 
 const firstWordLongerThan4Char = () => {
-  //
+	//
 };
 
 // Find Index
 
 const firstNumIndexDivisibleBy3 = () => {
-  //
+	//
 };
 
 const firstWordIndexLessThan2Char = () => {
-  //
+	//
 };
 
 // For Each
 
 const logValuesTimes3 = () => {
-  //
+	//
 };
 
 const logWordsWithExclamation = () => {
-  //
+	//
 };
 
 // Map
 
 const arrayValuesSquaredTimesIndex = () => {
-  //
+	//
 };
 
 const arrayWordsUpcased = () => {
-  //
+	//
 };
 
 // Some
 
 const areSomeNumsDivisibleBy7 = () => {
-  //
+	//
 };
 
 const doSomeWordsHaveAnA = () => {
-  //
+	//
 };
 
 module.exports = {
-  isEveryNumGreaterThan2,
-  isEveryWordShorterThan7,
-  arrayLessThan5,
-  arrayOddLengthWords,
-  firstValDivisibleBy4,
-  firstWordLongerThan4Char,
-  firstNumIndexDivisibleBy3,
-  firstWordIndexLessThan2Char,
-  logValuesTimes3,
-  logWordsWithExclamation,
-  arrayValuesSquaredTimesIndex,
-  arrayWordsUpcased,
-  areSomeNumsDivisibleBy7,
-  doSomeWordsHaveAnA,
+	isEveryNumGreaterThan2,
+	isEveryWordShorterThan7,
+	arrayLessThan5,
+	arrayOddLengthWords,
+	firstValDivisibleBy4,
+	firstWordLongerThan4Char,
+	firstNumIndexDivisibleBy3,
+	firstWordIndexLessThan2Char,
+	logValuesTimes3,
+	logWordsWithExclamation,
+	arrayValuesSquaredTimesIndex,
+	arrayWordsUpcased,
+	areSomeNumsDivisibleBy7,
+	doSomeWordsHaveAnA,
 };
